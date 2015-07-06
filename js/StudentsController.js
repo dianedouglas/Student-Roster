a48e47595b2d8c555436cdd74e873984af05cce4
Diane <diane.douglas1@gmail.com>
2015-07-06 12:05:54 -0700

    Add directive and method to delete a student.

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -3,5 +3,9 @@ studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
   $scope.addStudent = function() {
     $scope.students.push({ name: $scope.studentName });
     $scope.studentName = null;
-  }
+  };
+  $scope.deleteStudent = function(student) {
+    var index = $scope.students.indexOf(student)
+    $scope.students.splice(index, 1);
+  };
 });
